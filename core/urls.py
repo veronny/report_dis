44775a0d2738f6a3fb159b00eda96d33c2e20e7c
veronny393 <veronny393@hotmail.com>
2024-04-16 12:48:13 -0500

    reporte padron

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.BASE, name='BASE'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+]
